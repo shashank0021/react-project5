7b104619b54091c1f064e0908ef8db9bd7c55e62
Shashank Sharma <shashanksharma8868@gmail.com>
2024-05-30 23:50:31 +0530

    react ui web

---
+++ src/App.jsx
@@ -0,0 +1,227 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className="container">
+<div className="logo">
+<img src="https://flowbite.com/docs/images/logo.svg" />
+<h1>GeekFoods</h1>
+</div>
+
+<div className="nav-links">
+
+    <ul>
+        <li> <a href="#" id="Home">  Home </a></li>
+        <li><a href="#"> Quote </a> </li>
+        <li><a href="#"> Resturants </a> </li>
+        <li> <a href="#"> Foods </a> </li>
+        <li> <a href="#">Contact </a> </li>
+        
+
+    </ul>
+</div>
+<div className="btns">
+    <button>Get Started</button>
+</div>
+</div>
+      <div className="back">
+
+<h1>Lets Find at your </h1>
+<h2>Forever Food</h2>
+<p>my Geekfood place is the best place for the North indian and South Indian</p>
+<div className="btns">
+  <button className="red"> Search Now</button>
+  <button className="white">Know More</button>
+
+</div>
+      </div>
+
+      <div>
+      <div className="sections">
+        <div className="secImg">
+          <img src="https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" />
+        </div>
+        <div className="right-par">
+        <div className="right">
+          <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, debitis.</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, molestiae! Quidem est esse numquam odio deleniti, beatae, magni dolores provident quaerat totam eos, aperiam architecto eius quis quibusdam fugiat dicta.</p>
+        <button>Get In Touch</button>
+        </div>
+</div>
+      </div>
+    </div>
+
+    <div>
+        <div className="grid-section">
+            <div className="grid-content one">
+                <div className="grid-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam incidunt suscipit recusandae labore ratione doloremque, architecto et illo minus quo tenetur ducimus, voluptatibus repellendus fuga aperiam vel ab! Ipsam corrupti blanditiis dolorum! Officia assumenda rem nam, eveniet enim ad inventore laudantium est illum voluptatem quis.</p>
+                
+                </div>
+                <div className="divs">
+
+           
+                <div className="grid-img">
+                    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80" />
+
+                </div>
+<div className="grids">
+    
+<p>Gradies Lenmon</p>
+                   <p> Head Of SEO</p>
+</div>
+            </div>
+            </div>
+            <div className="grid-content two ">
+                <div className="grid-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, .</p>
+                
+                </div>
+                <div className="divs">
+
+           
+                <div className="grid-img">
+                    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80" />
+                 
+
+               
+                </div>
+<div className="grids">
+    
+<p>Gradies Lenmon</p>
+                   <p> Head Of SEO</p>
+</div>
+            </div>
+            </div>
+            <div className="grid-content  three">
+                <div className="grid-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam .</p>
+                
+                </div>
+                <div className="divs">
+
+           
+                <div className="grid-img">
+                    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80" />
+                 
+
+               
+                </div>
+<div className="grids">
+    
+<p>Gradies Lenmon</p>
+                   <p> Head Of SEO</p>
+</div>
+            </div>
+            </div>
+            <div className="grid-content four">
+                <div className="grid-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, molestiae aliquam .</p>
+                
+                </div>
+                <div className="divs">
+
+           
+                <div className="grid-img">
+                    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80" />
+                 
+
+               
+                </div>
+<div className="grids">
+    
+<p>Gradies Lenmon</p>
+                   <p> Head Of SEO</p>
+</div>
+            </div>
+            </div>
+            <div className="grid-content five ">
+                <div className="grid-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet, .</p>
+                
+                </div>
+                <div className="divs">
+
+           
+                <div className="grid-img">
+                    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80" />
+                 
+
+               
+                </div>
+<div className="grids">
+    
+<p>Gradies Lenmon</p>
+                   <p> Head Of SEO</p>
+</div>
+            </div>
+            </div>
+            <div className="grid-content six">
+                <div className="grid-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime minima dicta amet,</p>
+                
+                </div>
+                <div className="divs">
+
+           
+                <div className="grid-img">
+                    <img src="https://images.unsplash.com/photo-1603366445787-09714680cbf1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=944&q=80" />
+                 
+
+               
+                </div>
+<div className="grids">
+    
+<p>Gradies Lenmon</p>
+                   <p> Head Of SEO</p>
+</div>
+            </div>
+            </div>
+            
+        </div>
+    </div>
+``
+    <div>
+      <div className="footer">
+        <div className="footer-content">
+        
+          <p className="p">logoipsum</p>
+          <p className="lorem">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt consequuntur amet culpa cum itaque neque.</p>
+          <ul>
+            <li> <a href=""> About </a> </li>
+            <li> <a href=""> </a> </li>
+            <li> <a href=""> Careers </a> </li>
+            <li> <a href=""> History </a> </li>
+            <li> <a href=""> Services </a> </li>
+            <li> <a href=""> Project </a> </li>
+            <li> <a href=""> Blog </a> </li>
+          </ul>
+          <div className="social-links">
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faFacebook} size="2x" />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faTwitter} size="2x" />
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+export default App
